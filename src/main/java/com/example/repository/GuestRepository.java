de93eb1a5e366cc82a80c35e0e0d96952514d51f
Sen Lin <selin@selin-mn1.linkedin.biz>
2017-03-22 17:46:53 -0700

    Added source code from tutorial

---
+++ src/main/java/com/example/repository/GuestRepository.java
@@ -0,0 +1,10 @@
+package com.example.repository;
+
+import com.example.entity.Guest;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GuestRepository extends PagingAndSortingRepository<Guest, Long> {
+
+}
